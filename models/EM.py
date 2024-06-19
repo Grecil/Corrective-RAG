b415e0c18498b8357a63b8332a4b8ad39e56ab0b
Grecil <grecilunadkat@gmail.com>
2024-06-19 12:34:16 +0530

    using streamlit secrets to pass api key

--- models/EM.py
+++ models/EM.py
@@ -1,5 +1,6 @@
 from langchain_google_genai.embeddings import GoogleGenerativeAIEmbeddings
+import streamlit as st
 
 embedding = GoogleGenerativeAIEmbeddings(
-    model="models/text-embedding-004", google_api_key="key"
+    model="models/text-embedding-004", google_api_key=st.secrets["GEMINI_API_KEY"]
 )
--- models/LLM.py
+++ models/LLM.py
@@ -1,3 +1,6 @@
 from langchain_google_genai.chat_models import ChatGoogleGenerativeAI
+import streamlit as st
 
-llm = ChatGoogleGenerativeAI(model="gemini-1.5-flash", google_api_key="key")
+llm = ChatGoogleGenerativeAI(
+    model="gemini-1.5-flash", google_api_key=st.secrets["GEMINI-API-KEY"]
+)
